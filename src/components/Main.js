22fdf98626ce67dcc6e3325fdea2ab662af51311
Chris Mims <chritopherm970@aol.com>
2020-10-04 15:13:41 -0500

    Pushed up original files from past repos

---
+++ src/components/Main.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Main.css";
+
+function Main() {
+  return (
+    <main className="container">
+      <button class="button is-warning">Warning</button>
+      <button class="button is-warning">Warning</button>
+      <button class="button is-warning">Warning</button>
+      <button class="button is-warning">Warning</button>
+      <button class="button is-warning">Warning</button>
+      <button class="button is-warning">Warning</button>
+      <button class="button is-warning">Warning</button>
+    </main>
+  );
+}
+export default Main;
