91b006603e077062eb9e3cb7683bd5364f914a1f
omniclause <vaughnmurphy89@gmail.com>
2020-10-04 17:36:15 -0500

    got products rendering to table via json

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
 import React from "react";
+import "./Header.css";
 
 function Navbar() {
   return (
     <section className="hero">
       <div className="hero-body">
-        <div className="container">
+        <div>
           <h1 className="title">Toddler Shopper</h1>
           <button class="button is-primary">
             Checkout <i class="fa fa-shopping-cart" aria-hidden="true"></i>
