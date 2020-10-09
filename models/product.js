4af1ce30a0eb2a60e75c33ae5fb38e1ee5b71eca
Chris Mims <chritopherm970@aol.com>
2020-10-08 22:10:10 -0500

    Added seed folder and changed models folder to product.js

--- models/product.js
+++ models/product.js
@@ -1,14 +1,14 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const Product = new Schema(
+const productSchema = new Schema(
   {
       title: { type: String, required: true },
       image: { type: String, required: true },
       price: { type: String, required: true },
-      category: { type: String, required: true },
-  },
-  { timestamps: true },
-)
+      category: { type: String, required: true }
+  }
+);
+const Product = mongoose.model("Product", productSchema);
 
-module.exports = Post;
+module.exports = Product;
