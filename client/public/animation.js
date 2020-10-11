7f4e40795927f68e4432d5245fe90932071965e2
omniclause <vaughnmurphy89@gmail.com>
2020-10-11 15:39:58 -0500

    basic anime.js functioning

---
+++ client/public/animation.js
@@ -0,0 +1,56 @@
+const button = document.querySelector(".button");
+const card = document.querySelector(".card");
+
+// button animation
+const mouseHoverAnimation = () => {
+    anime({
+        targets: button,
+        width: '10rem',
+        scale: {
+            delay: 100,
+            value: 1.5
+        },
+        duration: 1500
+    })
+}
+
+const mouseOutAnimation = () => {
+    anime({
+        targets: button,
+        width: '7rem',
+        scale: {
+            delay: 100,
+            value: 1
+        },
+        duration: 1500
+    })
+}
+// card animation
+const cardHoverAnimation = () => {
+    anime({
+        targets: card,
+        // width: '10rem',
+        scale: {
+            delay: 100,
+            value: 1.15
+        },
+        duration: 1500
+    })
+}
+
+const cardOutAnimation = () => {
+    anime({
+        targets: card,
+        // width: '7rem',
+        scale: {
+            delay: 100,
+            value: 1
+        },
+        duration: 1500
+    })
+}
+
+button.addEventListener("mouseover", mouseHoverAnimation)
+button.addEventListener("mouseout", mouseOutAnimation)
+card.addEventListener("mouseover", cardHoverAnimation)
+card.addEventListener("mouseout", cardOutAnimation)
