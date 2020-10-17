59bdfe34da86eda2d84cf91b68b58ccb2369dd32
Chris Mims <chritopherm970@aol.com>
2020-10-17 11:22:11 -0500

    Second deployment

---
+++ client/build/precache-manifest.c01237b6ec13779b751aa029e8e843be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc3a949618fddd9b6d6625991a3361f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "430c703343ce6a67d4e7",
+    "url": "/static/css/main.d7b29248.chunk.css"
+  },
+  {
+    "revision": "fbb36c7c5f0a5679134a",
+    "url": "/static/js/2.ea450d29.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ea450d29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "430c703343ce6a67d4e7",
+    "url": "/static/js/main.3bba6268.chunk.js"
+  },
+  {
+    "revision": "16fae57dd86aee4551b9",
+    "url": "/static/js/runtime-main.63dba675.js"
+  },
+  {
+    "revision": "3df387d417ecab0bdd6cb9e22f97259a",
+    "url": "/static/media/delivery_truck.3df387d4.png"
+  }
+]);
