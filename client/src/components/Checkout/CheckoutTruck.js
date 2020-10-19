462a8d899b707b55e89d718df1b02cd074ff9ccd
Blackbird <ksilva.rod@gmail.com>
2020-10-18 21:42:20 -0500

    truck sound

--- client/src/components/Checkout/CheckoutTruck.js
+++ client/src/components/Checkout/CheckoutTruck.js
@@ -3,13 +3,23 @@ import "../Main.css";
 import "./Checkout.css";
 import truck from "./delivery_truck_2.png";
 import "./fireworks.css";
+import trucksound from "./truckhorn.mp3";
 
 class CheckoutTruck extends React.Component {
-  
-    render()
+
+  componentDidMount() {
+    const audioEl = document.getElementsByClassName("audio-element")[0]
+    audioEl.play()
+  }
+
+  render()
     {
       return (
         <main className="container">
+          <audio className="audio-element">
+            <source src={trucksound}></source>
+          </audio>
+
           <div className="card animate__animated animate__pulse animate__infinite	infinite" id="checkout-now"><div class="checkout-text">Your order is on it's way!</div></div>
           <img className="animate__animated animate__slideInLeft" id="truck" src={truck}></img>
           <div class = "fireWorkParticle"></div>
