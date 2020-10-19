0f841f80aadaea518336d47774c6e822f074f8b6
omniclause <vaughnmurphy89@gmail.com>
2020-10-19 03:30:29 -0500

    changed remove button to say cart. added star favicon to checkout table

--- client/src/components/Checkout/CheckoutCard.js
+++ client/src/components/Checkout/CheckoutCard.js
@@ -15,9 +15,9 @@ export default function CheckoutCard(props) {
         <img className="card-img-top" src={image} alt="page screenshot"></img>
       </td>
       <td>{title}</td>
-      <td>{rating}</td>
+      <td>{rating}<i class="fas fa-star"></i></td>
       <td>{rawPrice}</td>
-      <td><button class="button is-danger" onClick={()=> handleRemove(userId,productId) }> Remove from Wishlist!</button></td>
+      <td><button class="button is-danger" onClick={()=> handleRemove(userId,productId) }> Remove from Cart!</button></td>
        
     </tr>
   );
